10ddcc4d3f4e75081737ed72fd15e3fda4cd92a7
javierroldan2207 <javierroldan2207@gmail.com>
2024-09-25 09:38:43 +0200

    Exposicion primer trimestre

--- Programación/PYTHON/Inicio/holamundo.py
+++ Programación/PYTHON/Inicio/holamundo.py
@@ -5,4 +5,14 @@ print(17/4**2-3)
 print(9*2/3*10*3)
 print((9*5-6*6)**3//4*2)
 #// = Division sin decimales#
-
+'''
+comentario
+varias
+lineas
+'''
+#Formas de ajustar print.
+nombre="Javier"
+edad=23
+print("Hola",nombre," tines",edad,"años." )
+print("Hola %s tienes %s años." %(nombre,edad))
+print ("Hola {} tienes {} años.".format(nombre,edad))
