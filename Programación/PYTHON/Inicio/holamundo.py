b00ae9b17a955f5f3384afd8bf96a29f8d5580dc
javierroldan2207 <javierroldan2207@gmail.com>
2024-09-25 14:29:08 +0200

    programación 25/10

--- Programación/PYTHON/Inicio/holamundo.py
+++ Programación/PYTHON/Inicio/holamundo.py
@@ -1,3 +1,4 @@
+'''
 print("Hola mundo")
 #Expresiones logicas#
 print(33%5+35//4)
@@ -5,6 +6,7 @@ print(17/4**2-3)
 print(9*2/3*10*3)
 print((9*5-6*6)**3//4*2)
 #// = Division sin decimales#
+'''
 '''
 comentario
 varias
@@ -16,3 +18,4 @@ edad=23
 print("Hola",nombre," tines",edad,"años." )
 print("Hola %s tienes %s años." %(nombre,edad))
 print ("Hola {} tienes {} años.".format(nombre,edad))
+
