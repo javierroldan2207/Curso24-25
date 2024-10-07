53a4b4ac512990729a0ba5445d408c083fde03a2
javierroldan2207 <javierroldan2207@gmail.com>
2024-10-07 13:26:29 +0200

    Cambios

---
+++ Programación/PYTHON/Boletines/IntroducionBucles.py
@@ -0,0 +1,11 @@
+'''
+1. Escribe un programa que reciba dos números por teclado y que después muestre
+por pantalla los números que hay entre ellos. Por ejemplo, si se recibe 5 y 3 se
+mostrará: 3, 4, 5. Utiliza bucle for y while [for/while].
+'''
+
+'''
+2. Crea un programa que reciba un número y a continuación muestre la tabla de
+multiplicar de ese número. Utiliza sólo bucles de tipo while.
+
+'''
