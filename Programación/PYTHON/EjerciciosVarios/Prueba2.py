7633069a2b1ba551c0b2f4a387159ae0c5b6663f
javierroldan2207 <javierroldan2207@gmail.com>
2024-10-22 01:16:21 +0200

    Correccion

--- Programación/PYTHON/EjerciciosVarios/Prueba2.py
+++ Programación/PYTHON/EjerciciosVarios/Prueba2.py
@@ -1,13 +1,14 @@
+'''
 #21 de octubre 
 hombre=0
 mujer=0
 contador=0
 java=0
 python=0
-menu='''
+menu=
 1.Introducir datos.
 2.Salir
-'''
+
 print(menu)
 menu=int(input("Seleccione acción: "))
 while menu!=2:
@@ -35,13 +36,82 @@ while menu!=2:
      #Contador de empleados.   
     contador+=1
     
-    menu='''
+    menu=
         1.Introducir datos.
         2.Salir
-        '''
+        
     print(menu)
     menu=int(input("Seleccione acción: "))
     #Medias,cuentas y prints.
 media_edad=cont_edad/contador
 print(f"El {python/contador}% de empleados utiliza python, de los que {hombre} son hombres y {mujer} son mujeres y su edad media es de {media_edad}.")
 print(f"El {java/contador}% de empleados utiliza python, de los que {hombre} son hombres y {mujer} son mujeres y su edad media es de {media_edad}.")
+'''
+
+# Corrección en casa.
+total_empleados = 0
+cont_python = 0
+cont_java = 0
+edad_total_python = 0
+edad_total_java = 0
+cont_hombres_python = 0
+cont_mujeres_python = 0
+cont_hombres_java = 0
+cont_mujeres_java = 0
+
+tamaño_empresa = int(input("Ingrese el tamaño de la empresa (número de empleados): "))
+
+while total_empleados < tamaño_empresa:
+    edad = int(input("Ingrese la edad del empleado (18-65): "))
+    while edad < 18 or edad > 65:
+        print("Edad inválida. Debe estar entre 18 y 65.")
+        edad = int(input("Ingrese la edad del empleado (18-65): "))
+
+    sexo = input("Ingrese el sexo del empleado (h para hombre, m para mujer): ")
+    while sexo != 'h' and sexo != 'm':
+        print("Sexo inválido. Ingrese 'h' para hombre o 'm' para mujer.")
+        sexo = input("Ingrese el sexo del empleado (h para hombre, m para mujer): ")
+
+    lenguaje = input("Ingrese el lenguaje de programación habitual (java o python): ")
+    while lenguaje != 'java' and lenguaje != 'python':
+        print("Lenguaje inválido. Ingrese 'java' o 'python'.")
+        lenguaje = input("Ingrese el lenguaje de programación habitual (java o python): ")
+
+    total_empleados += 1
+    if lenguaje == 'python':
+        cont_python += 1
+        edad_total_python += edad
+        if sexo == 'h':
+            cont_hombres_python += 1
+        else:
+            cont_mujeres_python += 1
+    else:
+        cont_java += 1
+        edad_total_java += edad
+        if sexo == 'h':
+            cont_hombres_java += 1
+        else:
+            cont_mujeres_java += 1
+
+porcentaje_python = (cont_python / total_empleados) * 100
+porcentaje_java = (cont_java / total_empleados) * 100
+
+if cont_python > 0:
+    edad_media_python = edad_total_python / cont_python
+else:
+    edad_media_python = 0
+
+if cont_java > 0:
+    edad_media_java = edad_total_java / cont_java
+else:
+    edad_media_java = 0
+
+print(f"\n{porcentaje_python:.1f}% de empleados utilizan Python.")
+print(f"De los que, {cont_hombres_python} son hombres y {cont_mujeres_python} son mujeres.")
+print(f"La edad media es de {edad_media_python:.2f} años.")
+
+print(f"\n{porcentaje_java:.1f}% de empleados utilizan Java.")
+print(f"De los que, {cont_hombres_java} son hombres y {cont_mujeres_java} son mujeres.")
+print(f"La edad media es de {edad_media_java:.2f} años.")
+
+
