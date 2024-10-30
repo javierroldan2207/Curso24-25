740a76b7361dfce77f57f2d19bfbdca15ce30b95
javierroldan2207 <javierroldan2207@gmail.com>
2024-10-30 14:28:20 +0100

    progranación

--- Programación/PYTHON/EjerciciosVarios/palindromo.py
+++ Programación/PYTHON/EjerciciosVarios/palindromo.py
@@ -1,3 +1,4 @@
+'''
 def palindromo(cadena):
     for caracter in cadena:
         cadena_min=""
@@ -5,5 +6,39 @@ def palindromo(cadena):
             cadena_min=cadena.lower()
     return cadena_min== cadena[::-1]
 
-cadena="anaantootna ana"
+cadena="anaanto otnaana"
 print(palindromo(cadena))
+
+
+def palindromo1(cadena):
+    invertida=""
+    for letra in cadena:
+        invertida=letra+invertida
+    return invertida
+
+
+cadena="ana"
+print(palindromo1("Hola mi nombre es javier")) 
+'''
+cadena="jesus"
+#Inventir cadena
+def invertir(cadena):
+    invertida=""
+    for i in range(1,len(cadena)+1):
+        invertida+=cadena[-i]
+    assert(invertir("hola")=="aloh")
+#Quitar espacios
+def quitar_espacios(cadena):
+    filtrada=""
+    for letra in cadena:
+        if letra.isalnum():
+            filtrada+=letra
+            
+    return filtrada
+
+assert(quitar_espacios("hola, buenos dias")=="holabuenosdias")
+assert(quitar_espacios("hola buenos dias")=="holabuenosdias")
+#Verificar si es palindromo
+def es_palindromo(cadena):
+    return cadena==invertir
+
