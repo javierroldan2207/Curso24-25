4cb1806acabe390fd8fd7b1b8bf75f74aa595912
Javierroldan2207 <javierroldan2207@gmail.com>
2024-11-13 13:40:39 +0100

    optimizacion cifradoCesar

--- Programación/PYTHON/EjerciciosVarios/cifradoCesar.py
+++ Programación/PYTHON/EjerciciosVarios/cifradoCesar.py
@@ -4,27 +4,26 @@ def cifrar_caracter(caracter, desplazamiento):
     n_cifrado = (n_caracter + desplazamiento) % len(cadena) 
     return cadena[n_cifrado]
 
-assert cifrar_caracter("d",3)=="g"
+assert (cifrar_caracter("d",3) == "g")
 
 
 def cifrar_palabra(palabra,desplazamiento):
-    palabra_cifrada=""
+    palabra_cifrada = ""
     for letra in palabra:
         if letra in "abcdefghijklmnñopqrstuvwxyz":
-            palabra_cifrada+=cifrar_caracter(letra,desplazamiento)
-        else:
-            palabra_cifrada+=letra
+            palabra_cifrada += cifrar_caracter(letra,desplazamiento)
+        
     return palabra_cifrada
 
-assert cifrar_palabra("casado", 3) == "fdvdgr"
+assert (cifrar_palabra("casado", 3) == "fdvdgr")
 
-def comprovacion_cifrado(palabra1,palabra2,):
-    resultado="No son equivalentes"
+def comprovacion_cifrado(palabra,cifrado,):
+    resultado = "No son equivalentes"
     for desplazamiento in range(0,27):
-        palabra_cifrada=cifrar_palabra(palabra1,desplazamiento)
-        if palabra_cifrada==palabra2:
-            resultado=f"Son equivalentes y el cifrado es {desplazamiento}."
-            
+        palabra_cifrada = cifrar_palabra(palabra,desplazamiento)
+        if palabra_cifrada == cifrado:
+            resultado = f"Son equivalentes y el cifrado es {desplazamiento}."
+              
     return resultado
 
 print(comprovacion_cifrado("casado","fdvdgr"))
