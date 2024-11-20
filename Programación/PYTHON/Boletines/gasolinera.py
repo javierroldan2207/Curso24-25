037b49a9cc1042ff0d4aa3a39ac3683877a917c5
Javierroldan2207 <javierroldan2207@gmail.com>
2024-11-20 14:26:55 +0100

    gasolinera

---
+++ Programación/PYTHON/Boletines/gasolinera.py
@@ -0,0 +1,14 @@
+MENU = '''
+*************************************************
+1. Llenar tanque (diésel o gasolina)
+2. Asignar coche al surtidor y repostar.
+3. Consultar puntos de cliente.
+4. Comprobar histórico de ventas.
+5. Consultar el estado de los surtidores.
+6. Asignar precio a la gasolina/diésel.
+7. Cerrar el programa y salir.
+**************************************************
+'''
+
+def rellenar_tanque(cant,surtidor):
+    return 
