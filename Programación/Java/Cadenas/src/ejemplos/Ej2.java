0d7314b035f946b9bc24be79e2f81ff9b92d19cf
Javierroldan2207 <javierroldan2207@gmail.com>
2025-01-08 14:29:16 +0100

    java

---
+++ Programación/Java/Cadenas/src/ejemplos/Ej2.java
@@ -0,0 +1,13 @@
+package ejemplos;
+
+public class Ej2 {
+
+	public static void main(String[] args) {
+		String cadena = "interaccion";
+		System.out.println(cadena.contains("rac"));
+		System.out.println(cadena.indexOf("t"));
+		System.out.println(cadena.charAt(cadena.indexOf("z")));
+
+	}
+
+}
