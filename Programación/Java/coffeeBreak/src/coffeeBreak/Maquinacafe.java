8c9e88ae34a1566590c3c4fcc3411a90f733f4ee
javierroldan2207 <javierroldan2207@gmail.com>
2025-02-04 18:49:27 +0100

    Ejercicio coffeeBreak

---
+++ Programación/Java/coffeeBreak/src/coffeeBreak/Maquinacafe.java
@@ -0,0 +1,20 @@
+package coffeeBreak;
+
+public class Maquinacafe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private String id;
+	private double cafe;
+	private double leche;
+	private double chocolate;
+	private int cafesServicios;
+	private int capuchinosServicio;
+	private int chocolatesServivios;
+	private double importe;
+	
+
+}
